6183686fa21a883cf6a752384c32f12b1ae27dbf
mccampero <mccampero2@gmail.com>
2024-07-29 15:37:33 -0400

    build the movie list component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, createContext } from 'react';
+import { useState, createContext, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
 import Landing from './components/Landing/Landing';
@@ -6,30 +6,47 @@ import Dashboard from './components/Dashboard/Dashboard';
 import SignupForm from './components/SignupForm/SignupForm';
 import SigninForm from './components/SigninForm/SigninForm';
 import * as authService from '../src/services/authService'; // import the authservice
+import MovieList from './components/MovieList/MovieList';
+import * as movieService from './services/movieService';
+
 
 export const AuthedUserContext = createContext(null);
 
 const App = () => {
   const [user, setUser] = useState(authService.getUser()); // using the method from authservice
+  const [movies, setMovies] = useState([])
 
   const handleSignout = () => {
     authService.signout();
     setUser(null);
   };
 
+  useEffect(() => {
+    const fetchAllMovies = async () => {
+      const moviesData = await movieService.index();
+      setMovies(moviesData)
+    };
+    if (user) fetchAllMovies();
+  }, [user]);
+
   return (
     <>
       <AuthedUserContext.Provider value={user}>
         <NavBar user={user} handleSignout={handleSignout} />
         <Routes>
-          {user ? (
-            <Route path="/" element={<Dashboard user={user} />} />
-          ) : (
-            <Route path="/" element={<Landing />} />
-          )}
-          <Route path="/signup" element={<SignupForm setUser={setUser} />} />
-          <Route path="/signin" element={<SigninForm setUser={setUser} />} />
-        </Routes>
+  {user ? (
+    // Protected Routes:
+    <>
+      <Route path="/" element={<Dashboard user={user} />} />
+      <Route path="/movies" element={<MovieList movies={movies} />} />
+    </>
+  ) : (
+    // Public Route:
+    <Route path="/" element={<Landing />} />
+  )}
+  <Route path="/signup" element={<SignupForm setUser={setUser} />} />
+  <Route path="/signin" element={<SigninForm setUser={setUser} />} />
+</Routes>
       </AuthedUserContext.Provider>
     </>
   );
---
+++ src/components/MovieList/MovieList.jsx
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom';
+
+
+
+const MovieList = (props) => {
+    return  <main>
+    {props.movies.map((movie) => (
+      <Link key={movie._id} to={`/movies/${movie._id}`}>
+        <article>
+          <header>
+            <h2>{movie.title}</h2>
+            <p>
+              {movie.author.username} posted on 
+              {new Date(movie.createdAt).toLocaleDateString()}
+            </p>
+          </header>
+          <p>{movie.text}</p>
+        </article>
+      </Link>
+    ))}
+  </main>;
+};
+
+export default MovieList;

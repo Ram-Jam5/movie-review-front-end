2cb162ad93037784e858a07eae27e28f3dd7679a
akimender <akimender@gmail.com>
2024-08-02 14:36:29 -0400

    fixed add comment

--- src/components/CommentForm/CommentForm.jsx
+++ src/components/CommentForm/CommentForm.jsx
@@ -5,29 +5,39 @@ import * as movieService from '../../services/movieService';
 
 const CommentForm = (props) => {
   const [formData, setFormData] = useState({ text: '' });
-  const { movieId, reviewId, commentId} = useParams();
+  const { movieId, reviewId } = useParams();
   const navigate = useNavigate();
 
   useEffect(() => {
     const fetchCommentData = async () => {
       const movieData = await movieService.show(movieId);
-      const comment = movieData.reviews.find((review) => review._id === reviewId).comments.find((comment) => comment._id === commentId);
+      //const comment = movieData.reviews.find((review) => review._id === reviewId).comments.find((comment) => comment._id === commentId);
       if (comment) {
-        setFormData({ text: comment.text })
+        //setFormData({ text: comment.text })
       }
     }
-    fetchCommentData();
+    //fetchCommentData();
 
-  },[movieId, reviewId, commentId])
+  },[movieId, reviewId])
 
   const handleChange = (evt) => {
     setFormData({ ...formData, [evt.target.name]: evt.target.value });
   };
 
+  const handleAddComment = async (movieId, reviewId, commentFormData) => {
+    try {
+      const newComment = await movieService.createComment(movieId, reviewId, commentFormData)
+      setComments([newComment, ...comments])
+      navigate(`/movies/${movieId}/${reviewId}`)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   const handleSubmit = async (evt) => {
     evt.preventDefault();
-    if(movieId && reviewId && commentId) {
-      await movieService.updateComment(movieId, reviewId, commentId, formData)
+    if(movieId && reviewId) {
+      await handleAddComment(movieId, reviewId, formData)
     } else {
       await props.handleAddComment(formData)
     }

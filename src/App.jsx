1c415cbc0ddba7244463e9f1a23921fd8830efe0
akimender <akimender@gmail.com>
2024-08-02 01:03:23 -0400

    finished userprofile and reviewpage

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,12 @@ import * as movieService from './services/movieService';
 // import * as reviewService from './services/reviewService';
 import MovieDetails from './components/MovieDetails/MovieDetails';
 import MovieForm from './components/MovieForm/MovieForm';
-import ReviewDetails from './components/ReviewDetails/ReviewDetails'
+import ReviewDetails from './components/ReviewDetails/ReviewDetails';
 
 // import ReviewList from './components/ReviewList/ReviewList';
 
-import CommunityPage from './components/CommunityPage/CommunityPage'
+import CommunityPage from './components/CommunityPage/CommunityPage';
+import UserProfile from './components/UserProfile/UserProfile';
 import CommentForm from './components/CommentForm/CommentForm';
 
 export const AuthedUserContext = createContext(null);
@@ -26,16 +27,13 @@ const App = () => {
 
   const [movies, setMovies] = useState([]);
 
+  const [users, setUsers] = useState([]);
+
   //const [reviews, setReviews] = useState([]);
-  const [users, setUsers] = useState([])
 
   // const [reviews, setReviews] = useState([]);
 
-
- 
-
-
-
+  
 
   const handleSignout = () => {
     authService.signout();
@@ -119,6 +117,7 @@ const App = () => {
 
       <Route path="/movies/:movieId/edit" element={<MovieForm handleUpdateMovie={handleUpdateMovie} />} />
       <Route path="/users" element={<CommunityPage users={users}/>} />
+      <Route path="/users/:userId" element={<UserProfile />} />
     </>
   ) : (
     // Public Route:
--- src/services/movieService.js
+++ src/services/movieService.js
@@ -11,122 +11,124 @@ const index = async () => {
     }
   };
 
-  const show = async (movieId) => {
-    try {
-      const res = await fetch(`${BASE_URL}/${movieId}`, {
-        headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
-      });
-      if (!res.ok) {
-        throw new Error('Network response was not ok');
+// SHOW MOVIE
+const show = async (movieId) => {
+  try {
+    const res = await fetch(`${BASE_URL}/${movieId}`, {
+      headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+    });
+    if (!res.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return await res.json();
+  } catch (error) {
+    console.error('Error fetching movie:', error);
+    throw error;
+  }
+};
+
+const create = async (movieFormData) => {
+  try {
+    const res = await fetch(BASE_URL, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(movieFormData),
+    });
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// GET review
+const getReview = async (movieId, reviewId) => {
+  try {
+    const res = await fetch(`${BASE_URL}/${movieId}/${reviewId}`, {
+      headers: { Authorization: `Bearer ${localStorage.getItem('token')}`},
+    });
+    return res.json();
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const createComment = async (movieId, reviewId, commentFormData) => {
+  try {
+    const res = await fetch(`${BASE_URL}/${movieId}/${reviewId}/comments`, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(commentFormData),
+    });
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const deleteComment = async (movieId, reviewId, commentId) => {
+  try {
+    const res = await fetch(`${BASE_URL}/${movieId}/${reviewId}/comments/${commentId}`, {
+      method: 'DELETE',
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
       }
-      return await res.json();
-    } catch (error) {
-      console.error('Error fetching movie:', error);
-      throw error;
-    }
-  };
-
-  const create = async (movieFormData) => {
-    try {
-      const res = await fetch(BASE_URL, {
-        method: 'POST',
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem('token')}`,
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(movieFormData),
-      });
-      return res.json();
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  const getReview = async (movieId, reviewId) => {
-    try {
-      const res = await fetch(`${BASE_URL}/${movieId}/${reviewId}`, {
-        headers: { Authorization: `Bearer ${localStorage.getItem('token')}`},
-      });
-      return res.json();
-    } catch (error) {
-      console.log(error)
-    }
+    })
+    return res.json();
+  } catch (error) {
+    console.log(error)
   }
+}
 
-  const createComment = async (movieId, reviewId, commentFormData) => {
-    try {
-      const res = await fetch(`${BASE_URL}/${movieId}/${reviewId}/comments`, {
-        method: 'POST',
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem('token')}`,
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(commentFormData),
-      });
-      return res.json();
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  const deleteComment = async (movieId, reviewId, commentId) => {
-    try {
-      const res = await fetch(`${BASE_URL}/${movieId}/${reviewId}/comments/${commentId}`, {
-        method: 'DELETE',
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem('token')}`,
-        }
-      })
-      return res.json();
-    } catch (error) {
-      console.log(error)
-    }
+const updateComment = async (movieId, reviewId, commentId, commentFormData) => {
+  try {
+    const res = await fetch(`${BASE_URL}/${movieId}/${reviewId}/comments/${commentId}`, {
+      method: 'PUT',
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(commentFormData),
+    });
+    return res.json();
+  } catch (error) {
+    console.log(error)
   }
+}
 
-  const updateComment = async (movieId, reviewId, commentId, commentFormData) => {
-    try {
-      const res = await fetch(`${BASE_URL}/${movieId}/${reviewId}/comments/${commentId}`, {
-        method: 'PUT',
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem('token')}`,
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(commentFormData),
-      });
-      return res.json();
-    } catch (error) {
-      console.log(error)
-    }
+const deleteMovie = async (movieId) => {
+  try {
+    const res = await fetch(`${BASE_URL}/${movieId}`, {
+      method: 'DELETE',
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+      },
+    });
+    return res.json();
+  } catch (error) {
+    console.log(error);
   }
+};
 
-  const deleteMovie = async (movieId) => {
-    try {
-      const res = await fetch(`${BASE_URL}/${movieId}`, {
-        method: 'DELETE',
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem('token')}`,
-        },
-      });
-      return res.json();
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  async function update(movieId, movieFormData) {
-    try {
-      const res = await fetch(`${BASE_URL}/${movieId}/edit`, {
-        method: 'PUT',
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem('token')}`,
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(movieFormData),
-      });
-      return res.json();
-    } catch (error) {
-      console.log(error);
-    }
+async function update(movieId, movieFormData) {
+  try {
+    const res = await fetch(`${BASE_URL}/${movieId}/edit`, {
+      method: 'PUT',
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(movieFormData),
+    });
+    return res.json();
+  } catch (error) {
+    console.log(error);
   }
-  export { index, show, create, getReview, createComment, deleteComment, updateComment, deleteMovie, update };
+}
 
-
-
+export { index, show, create, getReview, createComment, deleteComment, updateComment, deleteMovie, update };
